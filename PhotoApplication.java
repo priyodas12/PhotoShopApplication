dc4aaf4ce5bb7a1cc7c0768b6a761939916c8ae4
Priyobrato Das <priyodas12@gmail.com>
2020-01-24 09:41:07 +0530

    Eureka discovery client

---
+++ PhotoApplication.java
@@ -0,0 +1,15 @@
+package com.workspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class PhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoApplication.class, args);
+	}
+
+}
